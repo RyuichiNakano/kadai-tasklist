87257ee2f86c6c5534ade095c6a7de5ab5f7e89e
ryuichinakano <blowinblues582@gmail.com>
2020-05-31 15:10:45 +0000

    controllerの書き換え

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,15 @@ class TasksController < ApplicationController
   end
 
   def show
+      @tasks = Task.find(params[:id])
   end
 
   def new
+      @tasks = Task.new
   end
 
   def create
+      @tasks = Task.new(tasks_params)
   end
 
   def edit
